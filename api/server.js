48040b3ee75ab042c70c4b723c95bb0b06735e81
JOHNELLY-001 <johnkiranga9@gmail.com>
2025-12-27 14:35:18 +0300

    First commit

---
+++ api/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const imeiRoute = require('../src/routes');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send("Welcome to IMEI VALIDATION APP");
+});
+
+app.use('/api', imeiRoute);
+
+app.listen(PORT, () => {
+  console.log(`IMEI VERIFICATION API running on http://localhost:${PORT}`);
+});
---
+++ src/routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const { fetchIMEIInfo } = require('../src/controller');
+
+router.post('/verify-imei', async (req, res) => {
+  const { imei } = req.body;
+
+  if (!imei) {
+    return res.status(400).json({ error: "IMEI is required" });
+  }
+
+  const data = await fetchIMEIInfo(imei);
+
+  if (data.error) {
+    return res.status(400).json(data);
+  }
+
+  res.json(data);
+});
+
+module.exports = router;
