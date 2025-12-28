153811f40584ef4be60db82f699779c9c1db98ce
JOHNELLY-001 <johnkiranga9@gmail.com>
2025-12-28 11:19:51 +0300

    Configured vue-router for ease navigation

--- frontend/imei-page/src/router/index.js
+++ frontend/imei-page/src/router/index.js
@@ -2,11 +2,15 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Payment from '../components/paymentPage.vue'
 import dashBoard from '../views/dashBoard.vue'
+import IMEIDocs from '../views/IMEIDocs.vue'
+import foooterPage from '../components/foooterPage.vue'
 
 const routes = [
   { path: '/home', name: 'Home', component: Home },
   { path: '/payment', name: 'Payment', component: Payment },
-  { path: '/', name: 'Dashboard', component: dashBoard }
+  { path: '/', name: 'Dashboard', component: dashBoard },
+  { path: '/docs', name: 'Docs', component: IMEIDocs },
+  { path: '/footer', name: 'Footer', component: foooterPage }
 ]
 
 const router = createRouter({
