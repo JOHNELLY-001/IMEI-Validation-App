4b962c2171771294592471673ffefcddd6ef1491
JOHNELLY-001 <johnkiranga9@gmail.com>
2025-12-28 11:19:06 +0300

    Added the footer component

---
+++ frontend/imei-page/src/components/foooterPage.vue
@@ -0,0 +1,14 @@
+<!-- Footer component -->
+ <!-- maintain the style and structure and colors -->
+<template>
+  <footer class="bg-transparent text-gray-700 py-4 mt-8">
+    <div class="container  text-center">
+      <p>&copy; 2026 IMEI Info. All rights reserved.</p>
+      <p>@ Cognitive Smart Technologies</p>
+      <!-- <p>
+        <a href="/privacy" class="underline">Privacy Policy</a> | 
+        <a href="/terms" class="underline">Terms of Service</a>
+      </p> -->
+    </div>
+  </footer>
+</template>
